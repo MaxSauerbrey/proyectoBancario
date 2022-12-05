68fc6d1048cea3be0a03e1c148191118620b5f34
Max <maxsauerbrey@gmail.com>
2022-12-05 19:46:37 -0300

    add methods in client and user service

--- src/Services/ClientService.java
+++ src/Services/ClientService.java
@@ -3,31 +3,17 @@ package Services;
 import Entities.Client;
 import Entities.User;
 
-import java.sql.ClientInfoStatus;
 import java.util.*;
 
 public class ClientService {
-    static Scanner input = new Scanner(System.in);
-    //    HashSet<Client> clients = new HashSet<Client>(10);
-    List<Client> clients = new ArrayList<Client>(10);
-//    Client [] clients = new Client [10];
+    static Scanner input = new Scanner(System.in).useDelimiter("\n");
+    static List<Client> clients = new ArrayList<>();
+
     UserService userServ = new UserService();
 
     public void createClient() {
         Client client = new Client();
-        Boolean err;
-        Integer age = 0;
-        do {
-            try {
-                System.out.print("Ingrese su edad: ");
-                age = input.nextInt();
-                err = false;
-            } catch (InputMismatchException e) {
-                System.out.println("Error. Debe ingresar un numero.");
-                err = true;
-                input.nextLine();
-            }
-        } while (err);
+        Integer age = enterAge();
 
         if (age > 17) {
             client.setName(validatorString("nombre"));
@@ -40,6 +26,23 @@ public class ClientService {
         } else {
             System.out.println("Para crear una cuenta debe ser mayor de edad.");
         }
+    }
+
+    public Integer enterAge() {
+        Integer age = 0;
+        Boolean err;
+        do {
+            try {
+                System.out.print("Ingrese su edad: ");
+                age = input.nextInt();
+                err = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Error. Debe ingresar un numero.");
+                err = true;
+                input.nextLine();
+            }
+        } while (err);
+        return age;
     }
 
     public void addClient(Client client) {
@@ -55,78 +58,146 @@ public class ClientService {
     public static String validatorString(String msj) {
         String str;
         System.out.print("Ingrese el " + msj + " del cliente: ");
-        str = input.nextLine();
+        str = input.next();
         while (str == null || str.isEmpty()) {
             System.out.print("Error. Ingrese el " + msj + " nuevamente: ");
-            str = input.nextLine();
+            str = input.next();
         }
         return str;
     }
 
     public void userMenu() {
-        String userName, userTarget;
-        Integer index = -1, depositIndex = -1;
-        double amount = 0;
+        String userName;
+        Integer index, opt = 0;
+        Boolean err;
 
-        //todo try-catch
+
         System.out.print("\nIngrese su nombre de usuario: ");
         userName = input.next();
 
-        System.out.println(userName);
+        index = getIndex(userName);
+        if (index == -1) System.out.println("\nNo existe un usuario con el alias ingresado.");
+        else {
+            System.out.println("\n\t\t  Bienvenido " +userName);
+            //todo ingresar contraseña
 
-        // todo hacer funcion
+            do {
+                do {
+                    System.out.print("\n\t\t\t Homebanking\n\n1- Ver saldo disponible.\n2- Ver sus datos personales.\n3- Deposito a otra cuenta.\n4- Salir.\n\nPor favor, elija una opción: ");
+                    try {
+                        opt = input.nextInt();
+                        err = false;
+                    } catch (InputMismatchException e) {
+                        System.out.println("Error. Solamente se pueden ingresar numeros.");
+                        err = true;
+                        input.nextLine();
+                    }
+                } while (err);
+                switch (opt) {
+                    case 1:
+                        System.out.println("Saldo disponible."); //todo ver saldo disponible
+                        break;
+                    case 2:
+                        System.out.println("Ver datos personales "); //todo
+                        break;
+                    case 3:
+                        depositMoney(index);
+                        break;
+                    case 4:
+                        System.out.println("\n\t\t  Adios " +userName);
+                        break;
+                    default:
+                        System.out.print("\nError.Ingrese una opcion correcta.\n");
+                }
+            } while (opt != 4);
+        }
+    }
+
+    private void depositMoney(Integer userIndex) {
+        Integer depositIndex;
+        String userTarget;
+        Double amount = 0D;
+
+        System.out.print("Ingrese el nombre de usuario a hacer el deposito: ");
+        userTarget = input.next();
+        depositIndex = getIndex(userTarget);
+
+
+        if (depositIndex != -1) {
+            amount = validateAmount(amount);
+            transaction(userIndex, depositIndex, userTarget, amount);
+            showClients();
+        } else if (userIndex == depositIndex) {
+            System.out.println("\nPara depositar en su cuenta elija la opcion autodeposito.");
+        } else {
+            System.out.println("\nNo existe un usuario con el alias ingresado.");
+        }
+
+
+    }
+
+    private void transaction(Integer userIndex, Integer depositIndex, String userTarget, Double amount) {
+        String ans;
+        if (!checkCountAmount(userIndex, amount)) {
+            System.out.println("Se depositaran $" + amount + " en la cuenta de " + userTarget + ".");
+            do {
+                System.out.println("Esta seguro de hacer esta operacion?(s/n): ");
+                ans = input.next();
+            } while (!ans.equalsIgnoreCase("s") && !ans.equalsIgnoreCase("n"));
+            if (ans.equalsIgnoreCase("s")) {
+                clients.get(userIndex).getUser().setSavingAccount(clients.get(userIndex).getUser().getSavingAccount() - amount);
+                clients.get(depositIndex).getUser().setSavingAccount(clients.get(depositIndex).getUser().getSavingAccount() + amount);
+                System.out.println("Operacion exitosa.");
+            } else {
+                System.out.println("Operacion cancelada.");
+            }
+        } else System.out.println("El saldo de su cuenta es inferior al monto de la transacción.");
+    }
+
+    private Boolean checkCountAmount(Integer userIndex, Double amount) {
+        Boolean err = false;
+        if (clients.get(userIndex).getUser().getSavingAccount() < amount) {
+            err = true;
+        }
+        return err;
+    }
+
+    private Double validateAmount(Double amount) {
+        boolean err = false;
+        do {
+            try {
+                System.out.print("Ingrese el monto a depositar: ");
+                amount = input.nextDouble();
+                err = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Error. Solamente se pueden ingresar números.");
+                err = true;
+                input.nextLine();
+            }
+        } while (err);
+        return amount;
+    }
+
+    private Integer getIndex(String user) {
+        Integer index = -1;
         for (Client u : clients) {
-            if (u.getUser().getUserName().equals(userName)) {
+            if (u.getUser().getUserName().equals(user)) {
                 index = clients.indexOf(u);
             }
         }
-
-        if (index == -1) {
-            System.out.println("\nNo existe un usuario de este banco con este alias");
-            return;
-        }
-
-        System.out.println("1 -Deposito a otra cuenta");
-        System.out.print("Ingrese el nombre de usuario a hacer el deposito: ");
-        userTarget = input.next();
-        System.out.println(userTarget);
-        for (Client u : clients) {
-            if (u.getUser().getUserName().equals(userTarget)) {
-                depositIndex = clients.indexOf(u);
-            }
-        }
-        System.out.println(depositIndex);
-        if (depositIndex == -1) {
-            System.out.println("El alias ingresado no pertenece a un usuario.");
-            return;
-        } else if (index == depositIndex) {
-            System.out.println("Para depositar en su cuenta elija la opcion autodeposito.");
-            return;
-        }
-
-        //todo try catch
-        System.out.print("Ingrese el monto a depositar: ");
-        amount = input.nextDouble();
-        //todo validacion si-no
-        System.out.println("Se depositaran " + amount + "$ en la cuenta " + userTarget + ". Esta seguro de hacer esta operacion? ");
-
-        clients.get(index).getUser().setSavingAccount(clients.get(index).getUser().getSavingAccount()-amount);
-        clients.get(depositIndex).getUser().setSavingAccount(clients.get(depositIndex).getUser().getSavingAccount()+amount);
-        showClients();
+        return index;
     }
 
 
     public void menu() {
-        addClient(new Client("Juan", "Perez", "1555584524", "juanperez@gmail.com", new User("juanpe", "juancho22", "15555", 500D)));
-        addClient(new Client("Laura", "Palmer", "1555584534", "laurapalmer@gmail.com", new User("laurap", "laurapalmera", "15555", 30D)));
-        addClient(new Client("Laura", "Palmer", "1555584533", "laurapalmera@gmail.com", new User("laurapa", "laurapalmeraa", "155535", 30D)));
-        addClient(new Client("Laura", "Palmer", "1555584533", "laurapalmera@gmail.com", new User("laurapa", "laurapalmeraa", "155535", 30D)));
+        addClient(new Client("Juan", "Perez", "1555584524", "juanperez@gmail.com", new User("juanpe", "juancho22", "1555555858321452", 500D)));
+        addClient(new Client("Laura", "Palmer", "1555584534", "laurapalmer@gmail.com", new User("laurap", "laurapalmera", "1555545812389517", 30D)));
+        addClient(new Client("Laura", "Palmera", "1555584533", "laurapalmera@gmail.com", new User("laurapa", "laurapalmeraa", "1558152336599875", 300D)));
         Integer opt = 0;
         Boolean err;
         System.out.println("\n\t.:Bienvenido al banco:.");
         do {
             do {
-
                 System.out.print("\n\t\t\tMENU\n\n1.Ingresar como usuario.\n2.Crear usuario.\n3.Salir.\n\n");
                 System.out.print("Por favor, elija una opción: ");
                 try {
@@ -140,11 +211,11 @@ public class ClientService {
             } while (err);
             switch (opt) {
                 case 1:
+                    showClients();
                     userMenu();
-//                    showClients();
                     break;
                 case 2:
-                    System.out.print("Ingrese su edad: ");
+
                     this.createClient();
                     break;
                 case 3:
--- src/Services/UserService.java
+++ src/Services/UserService.java
@@ -2,7 +2,6 @@ package Services;
 
 import Entities.Client;
 import Entities.User;
-import Services.ClientService;
 
 import java.util.Scanner;
 
@@ -12,39 +11,72 @@ public class UserService {
 
     public User createUser() {
         User user = new User();
+        String auxStringNumber;
 
-        user.setUserName(validatorString("nombre de usuario"));
+        usernameValidator(user);
         user.setPassword(validatorString("contraseña"));
-
-        //todo crear numero de tarjeta
-        user.setCardNumber(input.next());
-        //todo crear un aleatorio con cierto monto
+        cardValidator(user);
         user.setSavingAccount(0d);
         return user;
+    }
+
+    private String cardGenerator() {
+        String auxStringNumber;
+        Integer numeroAleatorio = (int) (Math.random()*99999999+1);
+        auxStringNumber=(String.valueOf(numeroAleatorio)+String.valueOf(numeroAleatorio));
+        return auxStringNumber;
+    }
+
+    private void cardValidator(User user){
+        String auxCardNumber;
+        do{
+            auxCardNumber=cardGenerator();
+        }while(uniqueNumberCard(auxCardNumber));
+        user.setCardNumber(auxCardNumber);
+    }
+
+    private void usernameValidator(User user) {
+        String auxUserName;
+        do {
+            auxUserName = (validatorString("nombre de usuario"));
+        } while (uniqueUserName(auxUserName));
+        user.setUserName(auxUserName);
     }
 
     public String validatorString(String msj) {
         String str;
         System.out.print("Ingrese el " + msj + " del cliente: ");
-        str = input.nextLine();
+        str = input.next();
 
         while (str == null || str.isEmpty()) {
             System.out.print("Error. Ingrese el " + msj + " nuevamente: ");
-            str = input.nextLine();
+            str = input.next();
         }
         return str;
     }
 
-//    public boolean uniqueUserName(String str) {
-//        Boolean match = false;
-//
-//        for (Client client : ClientService.clients) {
-//            if (str.equalsIgnoreCase(str)) {
-//                System.out.println("Ya existe un usuario con ese nombre.");
-//                match=true;
-//                break;
-//            }
-//        }
-//        return match;
-//    }
+    public boolean uniqueUserName(String userName) {
+        Boolean match = false;
+
+        for (Client client : ClientService.clients) {
+            if (client.getUser().getUserName().equalsIgnoreCase(userName)) {
+                System.out.println("Error. El nombre de usuario elegido ya esta utilizado.");
+                match = true;
+                break;
+            }
+        }
+        return match;
+    }
+
+    public boolean uniqueNumberCard (String numberCard){
+        Boolean match = false;
+        for(Client client: ClientService.clients){
+            if(client.getUser().getCardNumber().equals(numberCard)){
+                match= true;
+                break;
+            }
+        }
+        return match;
+    }
+
 }
